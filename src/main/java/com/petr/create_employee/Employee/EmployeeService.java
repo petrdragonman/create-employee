2660135c35195aa9405a26b4986f890a11b6c49a
petrdragonman <petrdragonman@me.com>
2025-03-18 12:30:08 +1100

    adds getAll, getById and deleteById endpoints

--- src/main/java/com/petr/create_employee/Employee/EmployeeService.java
+++ src/main/java/com/petr/create_employee/Employee/EmployeeService.java
@@ -1,15 +1,22 @@
 package com.petr.create_employee.Employee;
 
 import com.petr.create_employee.CreateEmployeeApplication;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
 
+    private final CreateEmployeeApplication createEmployeeApplication;
+
     private EmployeeRepository repo;
 
     EmployeeService(EmployeeRepository repo, CreateEmployeeApplication createEmployeeApplication) {
         this.repo = repo;
+        this.createEmployeeApplication = createEmployeeApplication;
     }
 
     public Employee createEmployee(CreateEmployeeDTO data) {
@@ -21,6 +28,23 @@ public class EmployeeService {
         newEmployee.setLastName(data.getLastName().trim());
         newEmployee.setStatus(data.getStatus());
         return this.repo.saveAndFlush(newEmployee);
+    }
+
+    public List<Employee> getAll() {
+        return this.repo.findAll();
+    }
+
+    public Optional<Employee> getById(Long id) {
+        return this.repo.findById(id);
+    }
+
+    public boolean deleteById(Long id) {
+        Optional<Employee> result = this.getById(id);
+        if (result.isEmpty()) {
+            return false;
+        }
+        this.repo.delete(result.get());
+        return true;
     }
     
 }
---
+++ src/main/java/com/petr/create_employee/common/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.petr.create_employee.common.exceptions;
+
+public class NotFoundException extends Exception {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
