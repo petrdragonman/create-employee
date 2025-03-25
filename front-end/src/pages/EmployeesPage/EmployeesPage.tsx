aaab6f03d1832b58167b71bf13c811acdcd03865
petrdragonman <petrdragonman@me.com>
2025-03-25 15:50:54 +1100

    sets up for uniquue key validations

--- front-end/src/pages/EmployeesPage/EmployeesPage.tsx
+++ front-end/src/pages/EmployeesPage/EmployeesPage.tsx
@@ -1,5 +1,5 @@
 import classes from "./EmployeesPage.module.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import EmployeesList from "../../components/EmployeesList";
 import Button from "../../components/Button/Button";
 import { useSelector } from "react-redux";
@@ -7,6 +7,7 @@ import { RootState } from "../../state/store";
 import { fetchEmployees, removeEmployee } from "../../state/employeeSlice";
 import { useAppDispatch } from "../../state/hooks";
 import { useNavigate } from "react-router";
+import Toast, { ToastType } from "../../components/Toast/Toast";
 
 const EmployeesPage = () => {
   const navigate = useNavigate();
@@ -31,9 +32,35 @@ const EmployeesPage = () => {
     navigate(`/employees/${id}`);
   };
 
+  const [toastMessage, setToastMessage] = useState<string | null>(null);
+  const [toastType, setToastType] = useState<ToastType>("success");
+
+  const showToast = (type: ToastType, message: string) => {
+    setToastType(type);
+    setToastMessage(message);
+  };
+
   return (
     <div className={classes.container}>
       <article className={classes.title}>create employee app</article>
+
+      <div>
+        <button onClick={() => showToast("success", "Success! Saved.")}>
+          Show Success
+        </button>
+        <button onClick={() => showToast("error", "Error! Failed.")}>
+          Show Error
+        </button>
+
+        <Toast
+          type={toastType}
+          message={toastMessage}
+          onClose={() => setToastMessage(null)}
+          //autoClose={true}
+          //autoCloseDuration={2000}
+        />
+      </div>
+
       <section className={classes.btn_create}>
         <Button variant="primary" onClick={handleClick}>
           Create new employee
